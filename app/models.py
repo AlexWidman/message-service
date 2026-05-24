55e9a85a5ebd2c16bff74710837e6cb388873a46
Alexander Widman <119602587+AlexWidman@users.noreply.github.com>
2026-05-24 15:50:05 +0200

    Create Message class for database rows

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+import datetime as dt
+
+from app.database import Base
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    id = Column(Integer, primary_key = True, index = True)
+    sender = Column(String, nullable = False)
+    recipient = Column(String, nullable = False)
+    content = Column(String, nullable = False)
+
+    timestamp = Column(DateTime, default = dt.datetime.now(dt.UTC)) # UTC for simplicity
+
+    is_read = Column(Boolean, default = False)
